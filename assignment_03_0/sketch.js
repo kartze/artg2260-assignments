4d8bc27d16c16b6afb846c8345760af955864114
Karisa Tzeng <stargazekin@gmail.com>
2018-02-02 22:42:02 -0500

    Clear Assignment 3 canvas, add notes on Assignment 2.1

--- assignment_03_0/sketch.js
+++ assignment_03_0/sketch.js
@@ -3,21 +3,5 @@ function setup() {
   canvas.parent('sketch-box');
   background(0);
 
-  stroke(255);
-  noFill();
-  for (var x = 0; x < 500; x += 50) {
-    for (var y = 0; y < 500; y += 50) {
-      if (y % 100) {
-        fill((x + y) * 30, (x + y) * 0.65, (x + y) * 0.45);
-        noStroke();
-        triangle(x - 25, y, x - 25, y + 50, x, y + 25);
-        rect(x - 50, y + 12.5, 25, 25);
-      } else {
-        fill((x + y) * 0.45, (x + y) * 0.65, (x + y) * 30);
-        noStroke();
-        triangle(x - 25, y, x - 25, y + 50, x, y + 25);
-        rect(x - 50, y + 12.5, 25, 25);
-      }
-    }
-  }
+
 }
