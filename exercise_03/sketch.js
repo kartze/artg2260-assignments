435d47e7a634095aec12ae6ba6ae64df4120c200
Karisa Tzeng <stargazekin@gmail.com>
2018-02-02 22:29:09 -0500

    Fix Exercise 3 margins

--- exercise_03/sketch.js
+++ exercise_03/sketch.js
@@ -16,7 +16,7 @@ function preload() {
 }
 
 function setup() {
-  var canvas = createCanvas(400, 400); // create a canvas at least 400 x 400
+  var canvas = createCanvas(500, 500); // create a canvas at least 400 x 400
   canvas.parent('sketch-box');
   xPos = width / 2; // set xPos to be half of the width
   yPos = height / 2; // set yPos to be half of the height
@@ -39,11 +39,11 @@ function draw() {
 
   // if we reach the edge of the canvas
   // turn around (toggle xSpeed negative to positive)
-  if (xPos + imageWidth / 2 >= width || xPos - imageWidth / 2 <= 0) {
+  if (xPos + imageWidth >= width || xPos <= 0) {
     xSpeed *= -1;
   }
   // same as above, toggle ySpeed if we hit the top or bottom
-  if (yPos + imageHeight / 2 >= height || yPos - imageHeight / 2 <= 0) {
+  if (yPos + imageHeight >= height || yPos <= 0) {
     ySpeed *= -1;
   }
 }
