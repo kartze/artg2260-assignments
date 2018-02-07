ba1e08a9ffc2991c1bb0f09e2a53a4e6a12e40f3
Karisa Tzeng <stargazekin@gmail.com>
2018-02-07 16:48:18 -0500

    Add Assignment 3.0

--- assignment_03_0/sketch.js
+++ assignment_03_0/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  var canvas = createCanvas(400, 400);
+  var canvas = createCanvas(500, 500);
   canvas.parent('sketch-box');
-  background(0);
+}
 
+function draw() {
+  fill(255, 5);
+  rect(0, 0, width, height);
 
+  transparency();
+  lines();
+}
+
+function transparency() {
+  var t = map(mouseX, 0, width, 0, height);
+  stroke(0, 100, 200, t);
+}
+
+function lines() {
+  for (var i = 0; i < 400; i++) {
+    var r = random(10);
+    strokeWeight(r);
+
+    var x = (frameCount + random(-i, i)) % width;
+    var y = i * 10;
+    line(250, 250, x, y);
+  }
 }
