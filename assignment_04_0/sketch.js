de2884888dcb6940cece137f79fcc69c3f86b7c1
Karisa Tzeng <stargazekin@gmail.com>
2018-02-09 13:35:54 -0500

    Fix Assignment 4.0
    First fully functioning game!

--- assignment_04_0/sketch.js
+++ assignment_04_0/sketch.js
@@ -1,32 +1,27 @@
-let num = 10;
-let xPos;
+let numCars = 10;
+let xPos = [];
 let yPos = [];
 let xSpeed = [];
-let spots = [];
+let maxSpeed = 7;
+let cars = [];
+let carWidth = 50;
 
-let img;
-let imgWidth = 50;
-let imgHeight = 50;
+let player1;
+let playerWidth = 50;
+let startXPos;
+let startYPos;
+
+let gamePieceHeight = 50; // same height for player and car gamepieces to maintain horizontal lines
 
 let gameState = 0;
-let time = 1000;
-
-function preload() {
-  img = loadImage('mario.png');
-}
+let time;
 
 function setup() {
-  var canvas = createCanvas(500, 500);
+  var canvas = createCanvas(600, 600);
   canvas.parent('sketch-box');
-  xPos = width / 2;
-  for (let i = 0; i < num; i++) {
-    yPos[i] = 25 + 50 * i;
-    if (i % 2 == 0) {
-      xSpeed[i] = random(1, 10);
-    } else {
-      xSpeed[i] = random(-10, -1);
-    }
-  }
+
+  startXPos = width / 2;
+  startYPos = height - gamePieceHeight / 2;
 }
 
 function draw() {
@@ -34,10 +29,12 @@ function draw() {
     startScreen();
   } else if (gameState == 1) {
     update();
-  } else if (gameState == 2) {
+  } else if (gameState == 2) { // when player is hit
     gameOver(1);
-  } else if (gameState == 3) {
+  } else if (gameState == 3) { // when time is up
     gameOver(2);
+  } else if (gameState == 4) {
+    win();
   }
 }
 
@@ -45,25 +42,47 @@ function startScreen() {
   background(255);
   text("FROGGER", 100, 225);
   text("Click to Begin", 100, 245);
-  for (let i = 0; i < num; i++) {
-    spots[i] = new Spot(xPos, yPos[i], xSpeed[i]);
+
+  for (let i = 0; i < numCars; i++) {
+    // initializes properties of each car
+    xPos[i] = random(0, width);
+    yPos[i] = gamePieceHeight * (1.5 + i);
+    if (i % 2 == 0) { // alternates car direction
+      xSpeed[i] = random(1, maxSpeed);
+    } else {
+      xSpeed[i] = random(-maxSpeed, -1);
+    }
+
+    // creates each car
+    cars[i] = new Car(xPos[i], yPos[i], xSpeed[i]);
   }
+
+  player1 = new Player(startXPos, startYPos);
+
+  time = 1000; // initializes countdown
 }
 
 function update() {
   background(255);
-  for (let i = 0; i < spots.length; i++) { // Make a for() loop to loop through each Spot
-    spots[i].move(); // Move each object
-    spots[i].display(); // Display each object
-    spots[i].check(); // Check for mouse overlap
+
+  for (let i = 0; i < numCars; i++) {
+    cars[i].move();
+    cars[i].display();
+    cars[i].checkHit();
   }
+
+  player1.display();
+  player1.checkWin();
 
   checkTime();
   time--;
 
   fill(0);
-  text("Playing", 10, 30);
-  text("Time: " + time, 10, 90);
+  text("Time: " + time, 25, 30);
+}
+
+function checkTime() {
+  if (time <= 0) gameState = 3; // time is up so game over
 }
 
 function gameOver(type) {
@@ -77,32 +96,36 @@ function gameOver(type) {
   text("Game Over", 100, 245);
 }
 
+function win() {
+  background(255);
+  fill(0);
+  text("You Win!", 100, 225);
+  text("Play Again?", 100, 245);
+}
+
 function mouseClicked() {
   if (gameState == 0) {
-    gameState = 1;
-  } else if (gameState == 2) {
-    gameState = 0;
-  } else if (gameState == 3) {
-    gameState = 0;
+    gameState = 1; // start to game
+  } else if (gameState == 2 || gameState == 3 || gameState == 4) {
+    gameState = 0; // game over to restart
   }
 }
 
-class Spot {
+class Car {
   constructor(_x, _y, _speed) {
     this.x = _x;
     this.y = _y;
     this.speed = _speed;
-    this.img = img;
-    this.width = 50;
-    this.height = 50;
+    this.width = carWidth;
+    this.height = gamePieceHeight;
   }
 
   move() {
     this.x += this.speed;
-    if (this.x - this.width >= width || this.x + this.width <= 0) {
-      if (this.speed > 0) {
+    if (this.x - this.width >= width || this.x + this.width <= 0) { // if car reaches wall
+      if (this.speed > 0) { // right-direction car reaches right wall, restart at left wall
         this.x = 0;
-      } else {
+      } else { // left-direction car reaches left wall, restart at right wall
         this.x = width;
       }
     }
@@ -110,16 +133,54 @@ class Spot {
 
   display() {
     fill(255);
-    image(img, this.x, this.y, this.width, this.height);
+    ellipse(this.x, this.y, this.width, this.height);
   }
 
-  check() {
-    if (abs(this.x - mouseX) < this.width && abs(this.y - mouseY) < this.height) {
-      gameState = 2;
+  checkHit() {
+    if (abs(player1.x - this.x) <= this.width / 2 + player1.width / 2 && this.y == player1.y) {
+      gameState = 2; // player touches car so game over
     }
   }
 }
 
-function checkTime() {
-  if (time <= 0) gameState = 3;
+class Player {
+  constructor(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+    this.width = playerWidth;
+    this.height = gamePieceHeight;
+  }
+
+  move(direction) {
+    if (direction == 1 && this.x < (width - this.width / 2)) {
+      this.x += this.width;
+    } else if (direction == 2 && this.x > this.width / 2) {
+      this.x -= this.width;
+    } else if (direction == 3 && this.y > this.height / 2) {
+      this.y -= this.height;
+    } else if (direction == 4 && this.y < (height - this.height / 2)) {
+      this.y += this.height;
+    }
+  }
+
+  display() {
+    fill(0);
+    ellipse(this.x, this.y, this.width, this.height);
+  }
+
+  checkWin() {
+    if (this.y == gamePieceHeight / 2) gameState = 4; // player reaches top wall so win
+  }
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    player1.move(1);
+  } else if (keyCode == LEFT_ARROW) {
+    player1.move(2);
+  } else if (keyCode == UP_ARROW) {
+    player1.move(3);
+  } else if (keyCode == DOWN_ARROW) {
+    player1.move(4);
+  }
 }
