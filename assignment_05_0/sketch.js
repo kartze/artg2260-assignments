20f2955327d90c15422045f0ae4d339749d9749f
Karisa Tzeng <stargazekin@gmail.com>
2018-02-22 14:31:28 -0500

    Add Assignment 5.0
    Game v2: Multiplayer
    Need to fix middle border.
    Next steps:
    - Add pause screen
    - Add scoreboard
    - Add points system

--- assignment_05_0/sketch.js
+++ assignment_05_0/sketch.js
@@ -18,31 +18,29 @@ let levelSpeedIncrease = 2; // how much speed increases each level
 let player1;
 let startXPos1;
 let startYPos1;
-let lives1 = 3;
+let level1;
 
 let player2;
 let startXPos2;
 let startYPos2;
-let lives2 = 3;
+let level2;
 
-let playerWidth = 50;
 let carWidth = 50;
+let playerWidth = 50;
 let gamePieceHeight = 50; // same height for player and car gamepieces to maintain horizontal lines
 
 let gameState = 0;
-// let time;
-let level;
-let level2;
+let time;
 
 function setup() {
   var canvas = createCanvas(1200, 600);
   canvas.parent('sketch-box');
 
   startXPos1 = width / 4;
-  startYPos1 = height - gamePieceHeight / 4;
+  startYPos1 = height - gamePieceHeight / 2;
 
   startXPos2 = 3 * width / 4;
-  startYPos2 = 3 * (height - gamePieceHeight) / 4;
+  startYPos2 = height - gamePieceHeight / 2;
 }
 
 function draw() {
@@ -50,14 +48,9 @@ function draw() {
     startScreen();
   } else if (gameState == 1) {
     update();
-  } else if (gameState == 2) { // when player is dead
-    gameOver(1);
-  } else if (gameState == 4) { // when player2 is dead
-    gameOver(3);
+  } else if (gameState == 2) { // when time is up
+    gameOver();
   }
-  // else if (gameState == 3) { // when time is up
-  //  gameOver(2);
-  // }
 }
 
 function mouseClicked() {
@@ -67,7 +60,7 @@ function mouseClicked() {
 
     if (gameState == 0) {
       gameState = 1; // start to game
-    } else if (gameState == 2 || gameState == 3 || gameState == 4) {
+    } else if (gameState == 2) {
       maxSpeed1 = 3; // reset speed
       maxSpeed2 = 3; // reset speed
       gameState = 0; // game over to restart
@@ -86,18 +79,18 @@ function startScreen() {
   background(255);
   line(0, 0, width, 0);
   line(0, 0, 0, height);
-  line(width / 2, 0, width / 2, height);
   line(width - 1, height, width - 1, 0);
   line(width, height - 1, 0, height - 1);
   text("CROSS THE ROAD", 100, 225);
   text("Click to Begin", 100, 245);
 
-  player1 = new Player(startXPos1, startYPos1, lives1, startXPos1);
-  player2 = new Player(startXPos2, startYPos2, lives2, startYPos2);
+  player1 = new Player1(startXPos1, startYPos1);
   createCars1();
+
+  player2 = new Player2(startXPos2, startYPos2);
   createCars2();
 
-  //  time = 1000; // initializes countdown
+  time = 1000; // initializes countdown
   level1 = 1;
   level2 = 1;
 }
@@ -105,22 +98,22 @@ function startScreen() {
 function createCars1() {
   for (let i = 0; i < numCars; i++) {
     // initializes properties of each car
-    xPos11[i] = random(0, width / 2);
-    yPos11[i] = gamePieceHeight * (1.5 + i);
+    xPos1[i] = random(0, width / 2);
+    yPos1[i] = gamePieceHeight * (1.5 + i);
     if (i % 2 == 0) { // alternates car direction
       xSpeed1[i] = random(minSpeed, maxSpeed1);
     } else {
       xSpeed1[i] = random(-maxSpeed1, -minSpeed);
     }
     // creates each car
-    cars1[i] = new Car(xPos1[i], yPos1[i], xSpeed1[i]);
+    cars1[i] = new Car1(xPos1[i], yPos1[i], xSpeed1[i]);
   }
 }
 
 function createCars2() {
   for (let i = 0; i < numCars; i++) {
     // initializes properties of each car
-    xPos2[i] = random(0, width / 2) + width / 2;
+    xPos2[i] = random(width / 2, width);
     yPos2[i] = gamePieceHeight * (1.5 + i);
     if (i % 2 == 0) { // alternates car direction
       xSpeed2[i] = random(minSpeed, maxSpeed2);
@@ -128,57 +121,56 @@ function createCars2() {
       xSpeed2[i] = random(-maxSpeed2, -minSpeed);
     }
     // creates each car
-    cars2[i] = new Car(xPos2[i], yPos2[i], xSpeed2[i]);
+    cars2[i] = new Car2(xPos2[i], yPos2[i], xSpeed2[i]);
   }
 }
 
 function update() {
-  background(255, 280 - level * 25, 280 - level * 25);
+  fill(255, 280 - level1 * 25, 280 - level1 * 25);
+  rect(0, 0, width / 2, height);
+  fill(255, 280 - level2 * 25, 280 - level2 * 25);
+  rect(width / 2, 0, width / 2, height);
   fill(0);
   line(0, 0, width, 0);
   line(0, 0, 0, height);
   line(width / 2, 0, width / 2, height);
   line(width - 1, height, width - 1, 0);
   line(width, height - 1, 0, height - 1);
-  text("Level: " + level1, 525, 20);
-  // text("Time: " + time, 25, 20);
-  text("Lives: " + player1.lives, 25, 20);
-  text("Level: " + level2, 525 + width / 2, 20);
-  // text("Time: " + time, 25, 20);
-  text("Lives: " + player2.lives, 25 + width / 2, 20);
+  textSize(12);
+  textAlign(LEFT);
+  fill(0, 0, 0);
+  text("Level: " + level1, 35, 575);
+  text("Level: " + level2, 35 + width / 2, 575);
+  textSize(200);
+  textAlign(CENTER);
+  fill(0, 0, 0, 51);
+  text(time, width / 2, height / 2);
 
   player1.display();
-  player1.checkDead();
   player1.checkWin();
 
   player2.display();
-  player2.checkDead();
   player2.checkWin();
 
   for (let i = 0; i < numCars; i++) {
     cars1[i].move();
     cars1[i].display();
     cars1[i].checkHit();
-  }
-
-  for (let i = 0; i < numCars; i++) {
     cars2[i].move();
     cars2[i].display();
     cars2[i].checkHit();
   }
 
-  //  checkTime();
-  //  time--;
+  checkTime();
+  time--;
 }
 
-class Player {
-  constructor(_x, _y, lives, startXPos, startYPos) {
+class Player1 {
+  constructor(_x, _y) {
     this.x = _x;
     this.y = _y;
     this.width = playerWidth;
     this.height = gamePieceHeight;
-    this.lives = lives;
-    this.startXPos = _startXPos, _startYPos;
   }
 
   display() {
@@ -187,7 +179,7 @@ class Player {
   }
 
   move(direction) {
-    if (direction == 1 && this.x < (width - this.width / 2)) { // move right but if you're not already at the right wall
+    if (direction == 1 && this.x < (width / 2 - this.width / 2)) { // move right but if you're not already at the right wall
       this.x += this.width;
     } else if (direction == 2 && this.x > this.width / 2) { // move left but if you're not already at the left wall
       this.x -= this.width;
@@ -198,48 +190,88 @@ class Player {
     }
   }
 
-  loseLife() {
-    this.lives--;
-  }
-
   reset() {
-    this.x = this.startXPos;
-    this.y = this.startYPos;
+    this.x = startXPos1;
+    this.y = startYPos1;
   }
 
   checkWin() {
-    if (this.y == gamePieceHeight / 2) levelUp(); // player reaches top wall so next level
-  }
-
-  checkDead() {
-    if (this.lives == 0) gameState = 2; // player is dead so game over
+    if (this.y == gamePieceHeight / 2) levelUp1(); // player reaches top wall so next level
   }
 }
 
-function levelUp() {
-  time = 1000; // re-initializes countdown
-  level++;
-  player1.lives = 3;
+class Player2 {
+  constructor(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+    this.width = playerWidth;
+    this.height = gamePieceHeight;
+  }
+
+  display() {
+    fill(0);
+    ellipse(this.x, this.y, this.width, this.height);
+  }
+
+  move(direction) {
+    if (direction == 1 && this.x < (width - this.width / 2)) { // move right but if you're not already at the right wall
+      this.x += this.width;
+    } else if (direction == 2 && this.x > (width / 2 + this.width / 2)) { // move left but if you're not already at the left wall
+      this.x -= this.width;
+    } else if (direction == 3 && this.y > this.height / 2) { // move up but if you're not already at the top wall
+      this.y -= this.height;
+    } else if (direction == 4 && this.y < (height - this.height / 2)) { // move down but if you're not already at the bottom wall
+      this.y += this.height;
+    }
+  }
+
+  reset() {
+    this.x = startXPos2;
+    this.y = startYPos2;
+  }
+
+  checkWin() {
+    if (this.y == gamePieceHeight / 2) levelUp2(); // player reaches top wall so next level
+  }
+}
+
+function levelUp1() {
+  level1++;
   player1.reset();
-  //	minSpeed+= levelSpeedIncrease; // increase speed next level
-  maxSpeed += levelSpeedIncrease; // increase speed next level
-  createCars();
+  maxSpeed1 += levelSpeedIncrease; // increase speed next level
+  createCars1();
+  gameState = 1;
+}
+
+function levelUp2() {
+  level2++;
+  player2.reset();
+  maxSpeed2 += levelSpeedIncrease; // increase speed next level
+  createCars2();
   gameState = 1;
 }
 
 function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
+  if (key == "d" || key == "D") {
     player1.move(1);
-  } else if (keyCode == LEFT_ARROW) {
+  } else if (key == "a" || key == "A") {
     player1.move(2);
-  } else if (keyCode == UP_ARROW) {
+  } else if (key == "w" || key == "W") {
     player1.move(3);
-  } else if (keyCode == DOWN_ARROW) {
+  } else if (key == "s" || key == "S") {
     player1.move(4);
+  } else if (keyCode == RIGHT_ARROW) {
+    player2.move(1);
+  } else if (keyCode == LEFT_ARROW) {
+    player2.move(2);
+  } else if (keyCode == UP_ARROW) {
+    player2.move(3);
+  } else if (keyCode == DOWN_ARROW) {
+    player2.move(4);
   }
 }
 
-class Car {
+class Car1 {
   constructor(_x, _y, _speed) {
     this.x = _x;
     this.y = _y;
@@ -255,9 +287,41 @@ class Car {
 
   move() {
     this.x += this.speed;
-    if (this.x - this.width >= width || this.x + this.width <= 0) { // if car reaches wall
+    if (this.x - this.width >= width / 2 || this.x + this.width <= 0) { // if car reaches wall
       if (this.speed > 0) { // right-direction car reaches right wall, restart at left wall
         this.x = 0;
+      } else { // left-direction car reaches left wall, restart at right wall
+        this.x = width / 2;
+      }
+    }
+  }
+
+  checkHit() {
+    if (abs(player1.x - this.x) <= this.width / 2 + player1.width / 2 && this.y == player1.y) {
+      player1.reset();
+    }
+  }
+}
+
+class Car2 {
+  constructor(_x, _y, _speed) {
+    this.x = _x;
+    this.y = _y;
+    this.speed = _speed;
+    this.width = carWidth;
+    this.height = gamePieceHeight;
+  }
+
+  display() {
+    fill(255);
+    ellipse(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    this.x += this.speed;
+    if (this.x - this.width >= width || this.x + this.width <= width / 2) { // if car reaches wall
+      if (this.speed > 0) { // right-direction car reaches right wall, restart at left wall
+        this.x = width / 2;
       } else { // left-direction car reaches left wall, restart at right wall
         this.x = width;
       }
@@ -265,29 +329,43 @@ class Car {
   }
 
   checkHit() {
-    if (abs(player1.x - this.x) <= this.width / 2 + player1.width / 2 && this.y == player1.y) {
-      player1.loseLife(); // player touches car so loses a life
-      player1.reset();
+    if (abs(player2.x - this.x) <= this.width / 2 + player2.width / 2 && this.y == player2.y) {
+      player2.reset();
     }
   }
 }
 
 function checkTime() {
-  if (time <= 0) gameState = 3; // time is up so game over
+  if (time <= 0) gameState = 2; // time is up so game over
 }
 
-function gameOver(type) {
-  background(255, 280 - level * 25, 280 - level * 25);
+function gameOver() {
+  fill(255, 280 - level1 * 25, 280 - level1 * 25);
+  rect(0, 0, width / 2, height);
+  fill(255, 280 - level2 * 25, 280 - level2 * 25);
+  rect(width / 2, 0, width / 2, height);
   line(0, 0, width, 0);
   line(0, 0, 0, height);
   line(width - 1, height, width - 1, 0);
   line(width, height - 1, 0, height - 1);
   fill(0);
-  if (type == 1) {
-    text("You're Dead!", 100, 225);
-  } else if (type == 2) {
-    text("Times Up!", 100, 225);
+  textSize(12);
+  textAlign(LEFT);
+  fill(0, 0, 0);
+  if (level1 > level2) {
+    line(width / 2, 0, width / 2, height);
+    text("You Won!", 100, 225);
+    text("Level: " + level1, 100, 245);
+    text("You Lose", 100 + width / 2, 225);
+    text("Level: " + level2, 100 + width / 2, 245);
+  } else if (level1 < level2) {
+    line(width / 2, 0, width / 2, height);
+    text("You Lose", 100, 225);
+    text("Level: " + level1, 100, 245);
+    text("You Won!", 100 + width / 2, 225);
+    text("Level: " + level2, 100 + width / 2, 245);
+  } else if (level1 == level2) {
+    text("Tie!", width / 2, 225);
+    text("Level: " + level1, width / 2, 245);
   }
-  text("Game Over", 100, 245);
-  text("Level: " + level, 100, 265);
 }
